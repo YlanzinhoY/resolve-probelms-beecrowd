7753d8d9e1074d40f8e8bdc0d933894eea4fcfdd
lanzinho <enzo.maxpayne@gmail.com>
2024-03-11 00:30:27 -0300

    bee1011.java

---
+++ bee1011.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class bee1011 {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        double raio = scan.nextDouble();
+        final double pi = 3.14159;
+        final double result = (4.0/3) * pi * Math.pow(raio, 3);
+        System.out.printf("VOLUME = %.3f", result);
+        scan.close();
+    
+    }
+}
